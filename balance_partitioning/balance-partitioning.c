611657b8f9e8b4269b1084483418af32e25156f8
shubham jawkar <84671309+shubhamjawkar1@users.noreply.github.com>
2021-11-29 15:28:26 +0530

    Create balance-partitioning.c

---
+++ balance_partitioning/balance-partitioning.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void balance(int n)
+{
+    int array[n],part1,part2;
+    for (int i = 0; i < n; i++)
+    {
+        int partition=6;               //n/2;
+        array[i]=((2*i)+4);
+        if (i<partition)
+        {
+            part1+=array[i];
+        }
+        else if(i>partition)
+        {
+            part2+=array[i];
+        }
+        
+        
+    }
+
+    printf("%d %d %d ",part2-part1);
+
+    
+
+}
+int main()
+{
+ balance(10);
+}
